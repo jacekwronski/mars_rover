5b733fee63da180d9f4433a13242a68eea40d0ed
Jacek Wronski <jacek.wronski.dev@gmail.com>
2021-07-12 22:14:43 +0200

    rover move around

---
+++ lib/mars_rover_server.ex
@@ -0,0 +1,70 @@
+defmodule MarsRoverServer do
+  use GenServer
+
+  def start_link([name]) do
+    GenServer.start_link(__MODULE__, [name], name: :rover)
+  end
+
+  def init([name]) do
+    Process.flag(:trap_exit, true)
+    {:ok, %{name: name, x: nil, y: nil, direction: nil}}
+  end
+
+  def land({direction, x, y}) do
+    GenServer.call(:rover, {:land, direction, x, y})
+  end
+
+  def land() do
+    GenServer.call(:rover, {:land, :north, 0, 0})
+  end
+
+  # def export(clinic_code, period) do
+  #   GenServer.cast(via_tuple(clinic_code, period), :export)
+  # end
+
+  def execute(cmd) do
+    GenServer.call(:rover, cmd)
+  end
+
+  def handle_call({:turn, turn_direction}, _from, %{direction: direction} = state) do
+    new_direction = turn(turn_direction, direction)
+    {:reply, :ok, %{state | x: 0, y: 0, direction: new_direction}}
+  end
+
+  def handle_call({:land, direction, x, y}, _from, state) do
+    {:reply, :ok, %{state | x: x, y: y, direction: direction}}
+  end
+
+  def handle_call(:get_state, _from, state) do
+    {:reply, {:ok, state}, state}
+  end
+
+  def handle_call({:move, :forward}, _from, %{direction: direction, x: x, y: y} = state) do
+    {new_x, new_y} = move(:forward, direction, {x, y})
+    {:reply, :ok, %{state | x: new_x, y: new_y}}
+  end
+
+  def handle_call({:move, :backward}, _from, %{direction: direction, x: x, y: y} = state) do
+    {new_x, new_y} = move(:backward, direction, {x, y})
+    {:reply, :ok, %{state | x: new_x, y: new_y}}
+  end
+
+  def move(:forward, :north, {x, y}), do: {x, y + 1}
+  def move(:forward, :south, {x, y}), do: {x, y - 1}
+  def move(:forward, :east, {x, y}), do: {x + 1, y}
+  def move(:forward, :west, {x, y}), do: {x - 1, y}
+  def move(:backward, :north, {x, y}), do: {x, y - 1}
+  def move(:backward, :south, {x, y}), do: {x, y + 1}
+  def move(:backward, :east, {x, y}), do: {x - 1, y}
+  def move(:backward, :west, {x, y}), do: {x + 1, y}
+
+
+  def turn(:left, :north), do: :west
+  def turn(:right, :north), do: :east
+  def turn(:left, :west), do: :south
+  def turn(:right, :west), do: :north
+  def turn(:left, :south), do: :west
+  def turn(:right, :south), do: :east
+  def turn(:left, :east), do: :north
+  def turn(:right, :east), do: :south
+end
--- test/mars_rover_test.exs
+++ test/mars_rover_test.exs
@@ -2,7 +2,127 @@ defmodule MarsRoverTest do
   use ExUnit.Case
   doctest MarsRover
 
-  test "greets the world" do
-    assert MarsRover.hello() == :world
+  setup do
+    registry = start_supervised!({MarsRoverServer, ["opportunity"]})
+    %{registry: registry}
+  end
+
+  test "land rover", %{registry: registry} do
+    assert MarsRoverServer.land() == :ok
+  end
+
+  test "turn rover right", %{registry: registry} do
+    MarsRoverServer.land()
+    MarsRoverServer.execute({:turn, :right})
+    MarsRoverServer.execute(:get_state)  == {:ok, %{name: "opportunity", x: 0, y: 0, direction: :east}}
+  end
+
+  test "turn rover left", %{registry: registry} do
+    MarsRoverServer.land()
+    MarsRoverServer.execute({:turn, :left})
+    MarsRoverServer.execute(:get_state)  == {:ok, %{name: "opportunity", x: 0, y: 0, direction: :west}}
+  end
+
+  test "turn rover :west turn left", %{registry: registry} do
+    MarsRoverServer.land({:west, 0, 0})
+    MarsRoverServer.execute({:turn, :left})
+    MarsRoverServer.execute(:get_state)  == {:ok, %{name: "opportunity", x: 0, y: 0, direction: :north}}
+  end
+
+  test "turn rover :west turn right", %{registry: registry} do
+    MarsRoverServer.land({:west, 0, 0})
+    MarsRoverServer.execute({:turn, :left})
+    MarsRoverServer.execute(:get_state)  == {:ok, %{name: "opportunity", x: 0, y: 0, direction: :south}}
+  end
+
+  test "turn rover :south turn left", %{registry: registry} do
+    MarsRoverServer.land({:west, 0, 0})
+    MarsRoverServer.execute({:turn, :left})
+    MarsRoverServer.execute(:get_state)  == {:ok, %{name: "opportunity", x: 0, y: 0, direction: :west}}
+  end
+
+  test "turn rover :south turn right", %{registry: registry} do
+    MarsRoverServer.land({:west, 0, 0})
+    MarsRoverServer.execute({:turn, :left})
+    MarsRoverServer.execute(:get_state)  == {:ok, %{name: "opportunity", x: 0, y: 0, direction: :east}}
+  end
+
+  test "turn rover :east turn left", %{registry: registry} do
+    MarsRoverServer.land({:west, 0, 0})
+    MarsRoverServer.execute({:turn, :left})
+    MarsRoverServer.execute(:get_state)  == {:ok, %{name: "opportunity", x: 0, y: 0, direction: :north}}
+  end
+
+  test "turn rover :east turn right", %{registry: registry} do
+    MarsRoverServer.land({:west, 0, 0})
+    MarsRoverServer.execute({:turn, :left})
+    MarsRoverServer.execute(:get_state)  == {:ok, %{name: "opportunity", x: 0, y: 0, direction: :south}}
+  end
+
+
+  test "turn rover :north turn 4 time right return to north", %{registry: registry} do
+    MarsRoverServer.land({:north, 0, 0})
+    MarsRoverServer.execute({:turn, :right})
+    MarsRoverServer.execute({:turn, :right})
+    MarsRoverServer.execute({:turn, :right})
+    MarsRoverServer.execute({:turn, :right})
+    MarsRoverServer.execute(:get_state)  == {:ok, %{name: "opportunity", x: 0, y: 0, direction: :north}}
+  end
+
+  test "turn rover :north turn 4 time left return to north", %{registry: registry} do
+    MarsRoverServer.land({:north, 0, 0})
+    MarsRoverServer.execute({:turn, :left})
+    MarsRoverServer.execute({:turn, :left})
+    MarsRoverServer.execute({:turn, :left})
+    MarsRoverServer.execute({:turn, :left})
+    MarsRoverServer.execute(:get_state)  == {:ok, %{name: "opportunity", x: 0, y: 0, direction: :north}}
+  end
+
+  test "rover at 2,2 face :north go forward positoi is 2,3", %{registry: registry} do
+    MarsRoverServer.land({:north, 2, 2})
+    MarsRoverServer.execute({:move, :forward})
+    assert MarsRoverServer.execute(:get_state) == {:ok, %{name: "opportunity", x: 2, y: 3, direction: :north}}
+  end
+
+  test "rover at 2,2 face :south go forward positoi is 2,1", %{registry: registry} do
+    MarsRoverServer.land({:south, 2, 2})
+    MarsRoverServer.execute({:move, :forward})
+    assert MarsRoverServer.execute(:get_state) == {:ok, %{name: "opportunity", x: 2, y: 1, direction: :south}}
+  end
+
+  test "rover at 2,2 face :north go backwad position is 2,1", %{registry: registry} do
+    MarsRoverServer.land({:north, 2, 2})
+    MarsRoverServer.execute({:move, :backward})
+    assert MarsRoverServer.execute(:get_state) == {:ok, %{name: "opportunity", x: 2, y: 1, direction: :north}}
+  end
+
+  test "rover at 2,2 face :south go backwad position is 2,3", %{registry: registry} do
+    MarsRoverServer.land({:south, 2, 2})
+    MarsRoverServer.execute({:move, :backward})
+    assert MarsRoverServer.execute(:get_state) == {:ok, %{name: "opportunity", x: 2, y: 3, direction: :south}}
+  end
+
+  test "rover at 2,2 face :east go farward position is 3,2", %{registry: registry} do
+    MarsRoverServer.land({:east, 2, 2})
+    MarsRoverServer.execute({:move, :forward})
+    assert MarsRoverServer.execute(:get_state) == {:ok, %{name: "opportunity", x: 3, y: 2, direction: :east}}
+  end
+
+  test "rover at 2,2 face :east go backward position is 3,2", %{registry: registry} do
+    MarsRoverServer.land({:east, 2, 2})
+    MarsRoverServer.execute({:move, :backward})
+    assert MarsRoverServer.execute(:get_state) == {:ok, %{name: "opportunity", x: 1, y: 2, direction: :east}}
+  end
+
+  test "rover at 2,2 face :west go foreward position is 1,2", %{registry: registry} do
+    MarsRoverServer.land({:west, 2, 2})
+    MarsRoverServer.execute({:move, :forward})
+    assert MarsRoverServer.execute(:get_state) == {:ok, %{name: "opportunity", x: 1, y: 2, direction: :west}}
+  end
+
+  test "rover at 2,2 face :west go backward position is 3,2", %{registry: registry} do
+    MarsRoverServer.land({:west, 2, 2})
+    MarsRoverServer.execute({:move, :backward})
+    assert MarsRoverServer.execute(:get_state) == {:ok, %{name: "opportunity", x: 3, y: 2, direction: :west}}
   end
 end
